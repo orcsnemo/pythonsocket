cd07b74f335f694b1f3f4842565600083b6ef92b
orcsnemo <wangww_orcs@163.com>
2014-08-26 17:01:37 +0800

    first

---
+++ firstserver.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import socket
+
+s=socket.socket()
+
+host = socket.gethostname()
+port=1234
+s.bind((host,port))
+
+s.listen(5)
+
+while True:
+	c,addr=s.accept()
+	print 'Got connection from', addr
+	c.send('Thank for connecting')
+	c.close()
+	
